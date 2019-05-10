455ad75dca8d286f67f98c3641173de8cefa6f43
xiong.xiao <xiong.xiao@aisppeech.com>
2019-05-10 12:11:58 +0800

    code style

--- js/kuanjia.js
+++ js/kuanjia.js
@@ -1,7 +1,6 @@
-$(function(){
-	$('.contter-p>a').click(function(){
-			console.log(1);
-			$('iframe').get(0).src = $(this).get(0).href;
-			return false;
-		});
+$(function () {
+	$('.contter-p>a').click(function () {
+		$('iframe').get(0).src = $(this).get(0).href;
+		return false;
+	});
 });
